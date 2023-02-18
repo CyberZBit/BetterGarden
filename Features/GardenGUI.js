fbb4c26a4dcb2114f0201e8715ed9e08dde2669b
CyberZ <othman.alhalab@gmail.com>
2023-02-18 02:29:02 +0100

    added some color to the texts

--- Features/GardenGUI.js
+++ Features/GardenGUI.js
@@ -1,8 +1,6 @@
 /// <reference types="../../CTAutocomplete" />
 import {nameInList, pogObject, replaceSpaces, replaceUnderscores} from "../utils/utils";
 
-
-const gui = new Gui()
 let mainGUIx = 60
 let mainGUIy = 70
 let mainGUIWidth = 250
@@ -16,8 +14,15 @@ function renderText(){
     const visitorName = Object.keys(textelement)[0];
     const visitorWants = textelement[visitorName].wants;
     const price_of_item_from_bz = textelement[visitorName].price
-    Renderer.drawString(`${replaceUnderscores(visitorName)}: ${visitorWants} ${price_of_item_from_bz}`, textX, textY);
+    Renderer.drawString(`${ChatLib.addColor("&e"+replaceUnderscores(visitorName)+"&r")}:${visitorWants} ${ChatLib.addColor("&6"+price_of_item_from_bz)}`, textX, textY);
   });
+}
+
+function noVis(){
+  if(pogObject.visitor.length == 0){
+    Renderer.drawString(ChatLib.addColor("&6No vistiors..."), 160, 100)
+    
+  }
 }
 
 
@@ -26,13 +31,13 @@ function mainGUI() {
   const extraHeightPerVisitor = 15; 
   const visitorCount = pogObject.visitor.length;
   const extraHeight = Math.min(visitorCount, 150) * extraHeightPerVisitor; 
-  
   Renderer.drawRect(Renderer.color(55, 55, 55), mainGUIx, mainGUIy, mainGUIWidth, baseHeight + extraHeight)
 }
 
 register('renderOverlay',()=>{
   mainGUI()
   renderText() 
+  noVis()
 }).setPriority(Priority.HIGHEST)
 
 
